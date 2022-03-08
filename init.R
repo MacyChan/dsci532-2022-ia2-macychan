6947b3c5a162a7fdb1156f396a0c84d345c81592
Macy Chan <macychan@student.ubc.ca>
2022-03-08 01:02:23 -0800

    Add remotes

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'dashHtmlComponents', 'plotly', 'purrr', 'tidyverse'))
+install.packages(c('dash', 'dashHtmlComponents', 'plotly', 'purrr', 'tidyverse', 'remotes'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
